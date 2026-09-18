12a6095fbbb7063db0023d1cb4fa922142f63183
connect2lakshya00-blip <connect2lakshya00@github.com>
2026-09-18 22:46:20 +0530

    Remove test files causing build errors

--- test-conn.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const db = new PrismaClient();
-db.$connect()
-  .then(() => { console.log('DB CONNECTED OK'); return db.$disconnect(); })
-  .catch(e => { console.log('DB ERROR:', e.message); process.exit(1); });
